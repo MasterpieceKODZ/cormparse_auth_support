52398e89dc76a5647daa521b63dfd20b58eea2ab
Gideon Faith Omorodion <grail.masterpiece@gmail.com>
2023-11-21 13:30:23 +0100

    added password reset (#7)
    * I just added a comment so i can trigger actions workflow
    
    * I added get-user-by-email endpoint and username and password auth alternative for nodemailer transport
    
    * I added the "get-email-from-cache" API endpoint, and made a change to the "create/username-n-pw/new-user" API endpoint to receive email address as part of the req body instead of receiving the redis cache key and fetching the email in the endpoint function before creating the user in db.
    
    * Added password reset email html filele, refactored the "/cache/save/email" endpoint to allow sending email for both verification and password reset

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/cache/save/email", async (req, res) => {
+	console.log("req body @ /cache/save/email", req.body);
 	const email = req.body.email;
 
 	try {
@@ -28,8 +29,13 @@ app.post("/cache/save/email", async (req, res) => {
 			},
 		});
 
-		if (emailIsUsed) {
-			res.status(400).send("email used");
+		//during email verification check if email has been used by another user
+		if (emailIsUsed && req.body.type == "verification") {
+			res.status(400).send("faulty email");
+			return;
+		} else if (!emailIsUsed && req.body.type == "reset_pw") {
+			//during password reset check if there is a user with the provided email address
+			res.status(400).send("faulty email");
 			return;
 		}
 	} catch (err) {
@@ -123,15 +129,29 @@ app.post("/cache/save/email", async (req, res) => {
 		const expString = `${expMonth} ${expDateTime.getDate()}, ${expDateTime.getFullYear()} at ${expDateTime.getHours()}:${expDateTime.getMinutes()}`;
 
 		// frontend service domain
-		const continueURL =
-			process.env.NODE_ENV == "production"
+		const frontendURI =
+          process.env.NODE_ENV == "production"
 				? "https://cormparse.ddns.net"
 				: "http://localhost:3000";
 
-		// fetch html email from file and format email template
+		// create continueURL based on email type (verication or password reset)
+		let continueURL;
 
+		if (req.body.type == "verification") {
+			continueURL = frontendURI + "/auth/finish-email-signup";
+		} else if (req.body.type == "reset_pw") {
+			continueURL = frontendURI + "/auth/reset-password";
+		}
+
+		// select email message based on type
+		const emailHTML =
+			req.body.type == "verification"
+				? `${process.cwd()}/verification.email.html`
+				: `${process.cwd()}/password.reset.html`;
+
+		// populate email template
 		const html = fs
-			.readFileSync(`${process.cwd()}/verification.email.html`, "utf8")
+			.readFileSync(emailHTML, "utf8")
 			.replace("{{redis-key}}", redisEmailKey)
 			.replace("{{link-expire-time}}", expString)
 			.replace("{{continue-url}}", continueURL);
@@ -141,14 +161,15 @@ app.post("/cache/save/email", async (req, res) => {
 			.sendMail({
 				from: '"Cormparse" cormparse@gmail.com',
 				to: email,
-				subject: "Verify Email",
+				subject:
+					req.body.type == "verification" ? "Verify Email" : "Reset Password",
 				html,
 				priority: "high",
 			})
 			.catch((e) => {
 				res.status(500).send("Failed");
 
-				// delete email address from cache if sending verification email fails
+				// delete email address from cache if sending email fails
 				redisClient.del(redisEmailKey);
 				console.log("email sending failed");
 				console.error(e);
@@ -159,14 +180,15 @@ app.post("/cache/save/email", async (req, res) => {
 			console.log(info);
 			res.sendStatus(200);
 		} else {
-			res.status(500).send("address rejected");
+			res.status(500).send("failed");
 
-			// delete email address from cache if sending verification email fails
+			// delete email address from cache if sending email fails
 			redisClient.del(redisEmailKey);
 		}
 	} else {
+    
 		// failed to cache email address
-		res.status(500).send("cache failed");
+		res.status(500).send("failed");
 	}
 });
 
@@ -225,7 +247,7 @@ app.post("/create/username-n-pw/new-user", async (req, res) => {
 		console.log("user created successfully");
 	} catch (error) {
 		// error while inserting user data into DB
-		res.status(500).send("error on create user");
+		res.status(500).send("failed");
 		console.log("error on create user");
 		console.error(error);
 	}
@@ -255,7 +277,7 @@ app.post("/get-user-by-email", async (req, res) => {
 });
 
 app.post("/get-email-from-cache", async (req, res) => {
-	console.log(req.body);
+	console.log("req body @ /get-email-from-cache", req.body);
 
 	try {
 		// get email from cache with provided key
@@ -278,6 +300,28 @@ app.post("/get-email-from-cache", async (req, res) => {
 		res.status(500).send("failed");
 		console.log("error on get-email-from-cache");
 		console.error(err);
+	}
+});
+
+app.post("/reset-password", async (req, res) => {
+	const body = req.body;
+
+	const saltNpw = await hashPassword(body.password);
+
+	const setPwRes = await prismaClient.user.update({
+		where: {
+			email: body.email,
+		},
+		data: {
+			passwordHash: saltNpw.hash,
+			salt: saltNpw.salt,
+		},
+	});
+
+	if (setPwRes) {
+		res.sendStatus(200);
+	} else {
+		res.sendStatus(500);
 	}
 });
 
